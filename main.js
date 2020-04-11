9c12330b85b040b15cb5ec5964cda513f9a41fb8
Christian Lohr <klausklapper@gmail.com>
2020-04-11 15:52:13 +0200

    Add service worker

--- main.js
+++ main.js
@@ -59,3 +59,19 @@ function shuffle(a) {
     }
     return a;
 }
+
+window.addEventListener('load', e => {
+    registerSW();
+});
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./sw.js');
+        } catch (e) {
+            alert('ServiceWorker registration failed. Sorry about that.');
+        }
+    } else {
+        document.querySelector('.alert').removeAttribute('hidden');
+    }
+}
